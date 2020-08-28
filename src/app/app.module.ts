905af89af2841e15dbd8a0cf64683d6dae12fc74
Brayan Castillo <bhcastillor@gmail.com>
2020-08-27 22:44:04 -0500

    layout login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, WelcomeComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  forma: FormGroup;
+  touchedUsername:boolean=false;
+  touchedPassword:boolean=false;
+  focusUsername:boolean=false;
+  focusPassword:boolean=false;
+  constructor(private fb: FormBuilder) {
+    this.crearFormulario();
+  }
+
+  ngOnInit(): void {}
+
+  crearFormulario() {
+    this.forma = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(5)]],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+    });
+  }
+  blurInput(input:string){
+    if(!this.forma.get(input).value){
+      if (input==='username'){
+        this.touchedUsername=false
+        this.focusUsername=false
+      }else{
+        this.touchedPassword=false
+        this.focusPassword=false
+      }
+    }
+  }
+  onFocusUsername (){
+    this.touchedUsername=true 
+    this.focusUsername =true
+  }
+  onFocusPassword (){
+    this.touchedPassword=true 
+    this.focusPassword =true
+  }
+  get UsernameNotValid() {
+    return (
+      this.forma.get('username').invalid && this.forma.get('username').touched
+    );
+  }
+  get PasswordNotValid() {
+    return (
+      this.forma.get('password').invalid && this.forma.get('password').touched
+    );
+  }
+}
